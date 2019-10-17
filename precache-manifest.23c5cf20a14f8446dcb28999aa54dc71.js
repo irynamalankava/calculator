9999a137b100408f3a4db049ab3ddc329d9ba839
ira <ira@ira>
2019-10-17 12:34:51 -0500

    Updates

---
+++ precache-manifest.23c5cf20a14f8446dcb28999aa54dc71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b103b3964c1e15115f5",
+    "url": "/static/css/main.5cf7e473.chunk.css"
+  },
+  {
+    "revision": "3b103b3964c1e15115f5",
+    "url": "/static/js/main.0640961b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05b7efd0579a36e32028",
+    "url": "/static/js/2.1b5ce0bc.chunk.js"
+  },
+  {
+    "revision": "fc13acb594c34e0864fd579cbc4e7490",
+    "url": "/index.html"
+  }
+];
